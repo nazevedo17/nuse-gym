c10084ec208400e4ca5fb429743f81987afed552
a18478 <a18478@users.noreply.github.com>
2020-12-21 20:32:46 +0000

    Mapping AutoMapper

---
+++ Nuse/Core/Code/AutoMapper/AutoMapperSetup.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Core.Data.Models;
+using Nuse.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nuse.Core.Code.AutoMapper
+{
+    public class AutoMapperSetup : Profile
+    {
+        public AutoMapperSetup()
+        {
+            #region Model To DTO
+
+            CreateMap<Customer, CustomerDTO>();
+
+            #endregion
+
+            #region DTO To Model
+
+            CreateMap<CustomerDTO, Customer>();
+
+            #endregion
+        }
+    }
+}
--- Nuse/Core/Startup.cs
+++ Nuse/Core/Startup.cs
@@ -35,7 +35,7 @@ namespace Nuse.Core
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAutoMapper(typeof(Startup));
+            services.AddAutoMapper(typeof(Code.AutoMapper.AutoMapperSetup));
 
             services.AddCors();
 
